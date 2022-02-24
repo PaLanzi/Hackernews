65b9e050e6c827d7cb683bdceefd0ac75edf45fc
PaLanzi <palanzillotta@googlemail.com>
2022-02-24 20:33:00 +0100

    search Bar

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useRef } from "react";
 
-function Form() {
+function Form(searchValue) {
+  const searchInput = useRef();
+  const searchIt = (e) => {
+    e.preventDefault();
+    searchValue(searchInput.current.value);
+  };
   return (
     <div>
-      <form>
-        <label>
-          <b>==></b>
-        </label>
-        <input type="text" placeholder="What you are looking for..."></input>
+      <form onSubmit={searchIt}>
+        <label></label>
+        <input
+          ref={searchInput}
+          type="text"
+          placeholder="What you are looking for..."
+        ></input>
         <button type="submit" className="button">
           Search
         </button>
